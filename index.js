98765333469bd6301d7050e55f7afa69d5396896
Ibrahim <ibitheboss786@outlook.com>
2024-03-04 22:33:24 +0000

    Quick Format

--- index.js
+++ index.js
@@ -4,31 +4,25 @@
 
 function contact(event) {
   event.preventDefault();
-  const loading = document.querySelector(' .about__overlay--loading')
-  const success = document.querySelector(' .about__overlay--success')
-  loading.classList += " about__overlay--visible"
+  const loading = document.querySelector(" .about__overlay--loading");
+  const success = document.querySelector(" .about__overlay--success");
+  loading.classList += " about__overlay--visible";
 
-  emailjs.sendForm(
-    "service_9rcnbdg",
-    "template_yybfzol",
-    event.target,
-    "wLsQljjo_DJbRAHvb"
-  ).then(() => {
-    loading.classList.remove("about__overlay--visible")
-    success.classList += " about__overlay--visible"
-  }).catch(() => {
-    loading.classList.remove("about__overlay--visible")
-    alert (
-        "The email service is temporarily unavailable, Please contact me directly on ibrahimahmed0@outlook.com"
+  emailjs
+    .sendForm(
+      "service_9rcnbdg",
+      "template_yybfzol",
+      event.target,
+      "wLsQljjo_DJbRAHvb"
     )
-  })
-}
-
-let isAboutOpen = false
-function toggleAbout() {
-    if (isAboutOpen) {
-        return document.body.classList.remove("about__open")
-    }
-    isAboutOpen = !isAboutOpen
-    document.body.classList += " about__open"
+    .then(() => {
+      loading.classList.remove("about__overlay--visible");
+      success.classList += " about__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("about__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable, Please contact me directly on ibrahimahmed0@outlook.com"
+      );
+    });
 }
